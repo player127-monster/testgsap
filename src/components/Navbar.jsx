5d2d17309de18c20be2dc5993b0babfff97e3205
player127-monster <sachindannagaraja@gmail.com>
2025-08-29 17:48:03 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Navbar() {
+  return (
+    <nav className='fixed top-0 left-0 z-50 md:p-9 p-3'>
+        <img src="/images/nav-logo.svg" alt='Logo' className='md:w-24 w-20'></img>
+    </nav>
+  )
+}
+
+export default Navbar
